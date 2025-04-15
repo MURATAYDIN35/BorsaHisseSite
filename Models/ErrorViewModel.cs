7484f7974e4650ba7f20a4ef884e2466af8cd5b2
MURATAYDIN35 <41143792+MURATAYDIN35@users.noreply.github.com>
2025-04-15 15:35:49 +0000

    Proje dosyalarını oluşturdum

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BorsaHisseSite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
