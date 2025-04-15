13b82d7f3920e82f98adb225bd05c4eb2c922c42
MURATAYDIN35 <41143792+MURATAYDIN35@users.noreply.github.com>
2025-04-15 16:03:47 +0000

    Eksik dosyalar eklendi: Model, Controller, View

---
+++ Models/HisseModel.cs
@@ -0,0 +1,11 @@
+namespace BorsaHisseSite.Models
+{
+    public class HisseModel
+    {
+        public string HisseAdi { get; set; }
+        public double Fiyat { get; set; }
+        public double DusanFiyat { get; set; }
+        public double YükselenFiyat { get; set; }
+        public double KapanisFiyati { get; set; }
+    }
+}
